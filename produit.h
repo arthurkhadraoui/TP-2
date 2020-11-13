7f37e5329ade9adc4cd6d95ca5f427e5a110b7ac
Strife <56599426+KevinLamblin@users.noreply.github.com>
2020-11-13 09:30:58 +0100

    Update produit.h
    Ajout des méthodes

--- produit.h
+++ produit.h
@@ -1,9 +1,39 @@
 #ifndef PRODUIT_H
 #define PRODUIT_H
 
+#include <iostream>
+
 namespace vente{
 
   class Produit{
+
+    public :
+    //Constructeurs de la classe Produit
+    Produit(string name);
+    Produit(string name, int quantity, double price);
+    Produit(string name, string description, int quantity, double price);
+
+    //Accesseurs des attributs de la classe Produit
+    void setDescription(string description);
+    void setPrice(double price);
+    void setQuantity(int quantity);
+
+    string getName();
+    string getDescription();
+    int getQuantity();
+    double getPrice();
+
+    
+
+    private :
+    //Nom du produit
+    string _name;
+    //Description du produit
+    string _description;
+    //Quantité disponible en stock
+    int _quantity;
+    //Prix unitaire du produit TTC
+    double _price;
 
   };
 }
