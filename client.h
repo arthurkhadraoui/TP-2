253e733364dce3973b896afc82f0595af0d8ec07
Strife <56599426+KevinLamblin@users.noreply.github.com>
2020-11-13 09:12:12 +0100

    Ajout des classes produits et client

---
+++ client.h
@@ -0,0 +1,11 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+namespace vente{
+
+    class Client{
+
+    };
+}
+
+#endif
---
+++ produit.h
@@ -0,0 +1,11 @@
+#ifndef PRODUIT_H
+#define PRODUIT_H
+
+namespace vente{
+
+  class Produit{
+
+  };
+}
+
+#endif
