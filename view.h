04c367ab26d7f1e0168d1d9830bdeb40bec9df97
Strife <56599426+KevinLamblin@users.noreply.github.com>
2021-01-27 14:00:56 +0100

    Commentaire de view

--- view.h
+++ view.h
@@ -1,18 +1,19 @@
 #ifndef VIEW_H
 #define VIEW_H
+
 #include <string.h>
 #include "observer.h"
-class View: public Observer
-{
-	
-private:
 
+class View: public Observer{
 
 public:
+	//Constructeur de la classe View
 	View();
-	
-	void clear() const;
 
+	//Nettayage de l'invite de commande
+	void clear() const;
+	//Affichage de la view
 	virtual void display() const;
+
 };
 #endif
