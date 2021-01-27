599efb68310b2c447b9e77949cc53342352c87a0
Arthur Khadraoui-Nicod <artoche10@yahoo.FR>
2021-01-27 18:53:56 +0100

    fixed system cls

--- view.cpp
+++ view.cpp
@@ -6,7 +6,7 @@ View::View(){
 }
 
 void View::clear() const{
-	system("CLS"); //Utilise la commande clear du système
+	//system("CLS"); //Utilise la commande clear du système
 }
 
 void View::display() const{
