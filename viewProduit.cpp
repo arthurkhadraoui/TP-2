0ab0857088314eb010c169268b1dbf1fd2f0da56
Strife <56599426+KevinLamblin@users.noreply.github.com>
2021-01-27 14:57:05 +0100

    Commentaires de la vue produit

--- viewProduit.cpp
+++ viewProduit.cpp
@@ -1,18 +1,17 @@
 #include "viewProduit.h"
 #include <string>
-ViewProduit::ViewProduit(Controller & _controller, vente::Magasin& magasin)
-	: View(), controller(_controller), _magasin(magasin)
-{
-	controller.addObserver(this);
+
+ViewProduit::ViewProduit(Controller & _controller, vente::Magasin& magasin)	: View(), controller(_controller), _magasin(magasin){
+	controller.addObserver(this); //Ajoute l'observer
 }
 
-void ViewProduit::notify()
-{
-	if (controller.getCurrentScreen() == 2)
-		display();
+void ViewProduit::notify(){
+	if (controller.getCurrentScreen() == 2) //Si l'on se trouve sur le menu produit
+		display(); //Affiche la page
 }
 
  void ViewProduit::addProduct(){
+	 //Saisie des informations du produit
 	std::cout<<"Veuillez saisir le nom du produit:"<<std::endl;
 	std::string name;
 	std::cin.ignore();
@@ -26,93 +25,102 @@ void ViewProduit::notify()
 	std::cout<<"Veuillez saisir le prix du produit:"<<std::endl;
 	double prix;
 	std::cin>>prix;
-	_magasin.vente::Magasin::addProduct(name,desc,quantity,prix);
-	notify();
+	_magasin.vente::Magasin::addProduct(name,desc,quantity,prix); //Ajout du produit au stock du magasin
+	notify(); //Mets à jour le MVC
 }
 
-
  void ViewProduit::showProduct(){
+	 //Choix du produit dont on veut les détails
 	std::cout<<"Veuillez saisir le nom du produit:"<<std::endl;
 	std::string name;
 	std::cin.ignore();
 	std::getline(std::cin,name);
-	_magasin.vente::Magasin::displayProduct(name);
+	_magasin.vente::Magasin::displayProduct(name); //Affichage du produit
 }
 
  void ViewProduit::updateProductQuantity(){
+	 //Choix du produit dont on veut modifier la quantité
 	std::cout<<"Veuillez saisir le nom du produit:"<<std::endl;
 	std::string name;
 	std::cin.ignore();
 	std::getline(std::cin,name);
+	//Saisie de la quantite
 	std::cout<<"Veuillez saisir la quantite du produit:"<<std::endl;
 	int quantity;
 	std::cin>>quantity;
-	_magasin.vente::Magasin::updateQuantity(name,quantity);
+	_magasin.vente::Magasin::updateQuantity(name,quantity); //Mets à jour la quantite du produit
 }
 
 void ViewProduit::addProductToCart(){
+	//Choix du client dont on modifie le panier
 	std::cout<<"Veuillez saisir le prenom du client:"<<std::endl;
 	std::string prenom;
 	std::cin>>prenom;
 	std::cout<<"Veuillez saisir le nom du client:"<<std::endl;
 	std::string nom;
 	std::cin>>nom;
+	//Choix du produit que l'on souhaite ajouter
 	std::cout<<"Veuillez saisir le nom du produit:"<<std::endl;
 	std::string name;
 	std::cin.ignore();
 	std::getline(std::cin,name);
-	_magasin.vente::Magasin::addProductCart(name,prenom,nom);
-	notify();
+	_magasin.vente::Magasin::addProductCart(name,prenom,nom); //Ajout du produit dans le panier du client
+	notify(); //Mets à jour le MVC
 }
 
 void ViewProduit::removeProductFromCart(){
+	//Choix du client dont on modifie le panier
 	std::cout<<"Veuillez saisir le prenom du client:"<<std::endl;
 	std::string prenom;
 	std::cin>>prenom;
 	std::cout<<"Veuillez saisir le nom du client:"<<std::endl;
 	std::string nom;
 	std::cin>>nom;
+	//Choix du produit que l'on souhaite retirer
 	std::cout<<"Veuillez saisir le nom du produit:"<<std::endl;
 	std::string name;
 	std::cin.ignore();
 	std::getline(std::cin,name);
-	_magasin.vente::Magasin::removeProductCart(name,prenom,nom);
-	notify();
+	_magasin.vente::Magasin::removeProductCart(name,prenom,nom); //Suppression du produit du panier du client
+	notify(); //Mets à jour le MVC
 }
 
 void ViewProduit::updateProductQuantityCart(){
+	//Choix du client dont on modifie le panier
 	std::cout<<"Veuillez saisir le prenom du client:"<<std::endl;
 	std::string prenom;
 	std::cin>>prenom;
 	std::cout<<"Veuillez saisir le nom du client:"<<std::endl;
 	std::string nom;
 	std::cin>>nom;
+	//Choix du produit dont on souhaite modifier la quantité
 	std::cout<<"Veuillez saisir le nom du produit:"<<std::endl;
 	std::string name;
 	std::cin.ignore();
 	std::getline(std::cin,name);
+	//Saisie de la quantité
 	std::cout<<"Veuillez saisir la quantite du produit:"<<std::endl;
 	int quantity;
 	std::cin>>quantity;
-	_magasin.vente::Magasin::updateProductQuantityCart(name,quantity,prenom,nom);
-	notify();
+	_magasin.vente::Magasin::updateProductQuantityCart(name,quantity,prenom,nom); //Mise à jour de la quantité dans le panier du client
+	notify(); //Mets à jour le MVC
 }
 
 void ViewProduit::clearCart(){
+	//Choix du clinet dont on souhaite vider le panier
 	std::cout<<"Veuillez saisir le prenom du client:"<<std::endl;
 	std::string prenom;
 	std::cin>>prenom;
 	std::cout<<"Veuillez saisir le nom du client:"<<std::endl;
 	std::string nom;
 	std::cin>>nom;
-	_magasin.vente::Magasin::clearCart(prenom,nom);
-	notify();
+	_magasin.vente::Magasin::clearCart(prenom,nom); //Suppression de tous les produits du panier du client
+	notify(); //Mets à jour le MVC
 }
 
-
-
  void ViewProduit::display(){
-	_magasin.vente::Magasin::displayAllProducts();
+	_magasin.vente::Magasin::displayAllProducts(); //Affiche tous les produits disponible dans le stock du magasin
+	//Affichage des actions possibles
 	std::cout<<std::endl;
 	for (int i=0; i<75;i++){
 			std::cout<<"-";
@@ -183,17 +191,17 @@ void ViewProduit::clearCart(){
 			std::cout<<"-";
 		}
 		std::cout<<std::endl;
-
+		//Choix de la prochaine action
 		int res;
 		std::cin.clear();
 		std::cin >> res;
 
-		while (res!=0 && res!=1 && res!=2 && res!=3 && res!=4 && res!=5 && res!=6 && res!=7  ){
-		std::cout<<"Veuillez selectionner une option valide:"<<std::endl;
+		while (res!=0 && res!=1 && res!=2 && res!=3 && res!=4 && res!=5 && res!=6 && res!=7  ){ //Si l'action saisie n'existe pas
+		std::cout<<"Veuillez selectionner une option valide:"<<std::endl; //On demande au client de saisir une option valide
 		std::cin >> res;
 	}
 
-		switch(res){
+		switch(res){ //Effectue l'action choisie
 			case 0: controller.setScreen(0);break;
 			case 1: addProduct();break;
 			case 2: showProduct();break;
@@ -204,4 +212,3 @@ void ViewProduit::clearCart(){
 			case 7:clearCart();break;
 		}
 }
-
--- viewProduit.h
+++ viewProduit.h
@@ -1,36 +1,40 @@
 #ifndef VIEWPRODUIT_H
 #define VIEWCPRODUIT_H
+
 #include "observer.h"
 #include "view.h"
 #include "controller.h"
 #include "magasin.h"
 
-class ViewProduit: public View
-{
-private:
-Controller& controller;
-vente::Magasin& _magasin;
+class ViewProduit: public View{
 
-public:
-	
-	ViewProduit(Controller& _controller,vente::Magasin& magasin);
+	public:
+		//Constructeur de la classe viewProduit
+		ViewProduit(Controller& _controller,vente::Magasin& magasin);
 
-	virtual void notify();
+		//Mets à jour le MVC
+		virtual void notify();
+		//Affiche le menu produit
+		virtual void display();
+		//Ajoute un produit au stock
+		virtual void addProduct();
+		//Affiche le stock
+		virtual void showProduct();
+		//Ajoute un produit au panier du client
+		virtual void addProductToCart();
+		//Enlève un produit du panier du client
+		virtual void removeProductFromCart();
+		//Mets à jour la quantité d'un produit dans le panier du client
+		virtual void updateProductQuantityCart();
+		//Vide le panier du client
+		virtual void clearCart();
+		//Mets à jour la quantité d'un produit dans le stock
+		virtual void updateProductQuantity();
 
-	virtual void display();
-
-	virtual void addProduct();
-
-	virtual void showProduct();
-
-	virtual void addProductToCart();
-
-	virtual void removeProductFromCart();
-
-	virtual void updateProductQuantityCart();
-
-	virtual void clearCart();
-
-	virtual void updateProductQuantity();
+	private:
+		//Controller du menu produit
+		Controller& controller;
+		//Instance du magasin
+		vente::Magasin& _magasin;
 };
 #endif
